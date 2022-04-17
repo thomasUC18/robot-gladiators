5254a4c25586441ec076164634e97aca15db88ca
Jade Thomas <jade.thomas@uconn.edu>
2022-04-16 21:52:20 -0400

    initial html and javascript

---
+++ assets/js/game.js
@@ -0,0 +1,17 @@
+function fight () {
+    window.alert ("The fight has begun!");
+}
+
+// fight ();
+
+window.prompt("What is your robot's name?");
+
+var playerName = window.prompt("What is your robot's name?");
+console.log(playerName);
+
+console.log("This logs a string, good for leaving yourself a message");
+console.log(10 + 10);
+console.log("Our robot's name is " + playerName);
+
+var name = "your name";
+console.log(name);
